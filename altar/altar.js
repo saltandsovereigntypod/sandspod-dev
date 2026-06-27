a6af6ab9fdb5419e58bcc24e94e1d5d3a7acb896
saltandsovereigntypod <saltandsovereigntybusiness@gmail.com>
2026-06-27 16:56:14 -0500

    Update altar.js

--- altar/altar.js
+++ altar/altar.js
@@ -909,3 +909,13 @@ window.addEventListener("resize", () => {
 });
 
 updateEmptyMessage();
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("./service-worker.js")
+      .catch((error) => {
+        console.warn("Service worker registration failed.", error);
+      });
+  });
+}
